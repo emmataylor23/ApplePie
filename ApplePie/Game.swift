55640c273c5e5fd62fba4f45531e2c9941ae29cd
emmataylor23 <emmanorcross04@yahoo.com>
2019-05-29 10:43:02 -0400

    apple pie app

---
+++ ApplePie/Game.swift
@@ -0,0 +1,15 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by  on 5/29/19.
+//  Copyright © 2019 EmmasApps. All rights reserved.
+//
+
+import Foundation
+
+struct Game
+{
+    var word: String
+    var incorrectMovesRemaining : Int
+}
--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -5,14 +5,39 @@
 //  Created by  on 5/29/19.
 //  Copyright © 2019 EmmasApps. All rights reserved.
 //
-
+// i stopped at the top of page 283 in the tutorial but i think i got everything before that
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var lettersButtons: [UIButton]!
+    var listOfWords = ["apple", "zoo", "beach", "doctor", "restaurant"]
+    let incorrectMovesAllowed = 7
+    var totalWins = 0
+    var totalLosses = 0
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton)
+    {
+        sender.isEnabled = false
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       newRound()
+    }
+    
+    var currentGame: Game!
+    
+    func newRound()
+    {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
     }
 
 
